2fb9ae7c6ba93020322337c15c51faf96bc39f32
milena <milenasantos@bemol.com.br>
2021-08-29 18:50:05 -0400

    Extraindo widgets flexíveis da class FormularioTransferencia

--- lib/main.dart
+++ lib/main.dart
@@ -21,8 +21,9 @@ class ByteBanckApp extends StatelessWidget {
 //Tela de Formulário de transferencia
 class FormularioTransferencia extends StatelessWidget {
   final TextEditingController _controladorCampoNumeroConta =
-      TextEditingController();
-  final TextEditingController _controladorCampoValor = TextEditingController();
+      new TextEditingController();
+  final TextEditingController _controladorCampoValor =
+      new TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -32,50 +33,63 @@ class FormularioTransferencia extends StatelessWidget {
       ),
       body: Column(
         children: [
-          Padding(
-            padding: const EdgeInsets.fromLTRB(24.0, 8.0, 24.0, 8.0),
-            child: TextField(
-              controller: _controladorCampoNumeroConta,
-              style: TextStyle(
-                fontSize: 20.0,
-              ),
-              decoration: InputDecoration(
-                labelText: 'Número da Conta',
-                hintText: '0000',
-              ),
-              keyboardType: TextInputType.number,
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.fromLTRB(24.0, 8.0, 24.0, 8.0),
-            child: TextField(
-              controller: _controladorCampoValor,
-              style: TextStyle(
-                fontSize: 20.0,
-              ),
-              decoration: InputDecoration(
-                icon: Icon(Icons.monetization_on),
-                labelText: 'Valor',
-                hintText: '0,00',
-              ),
-              keyboardType: TextInputType.number,
-            ),
-          ),
+          Editor(
+              controlador: _controladorCampoNumeroConta,
+              rotulo: 'Número Conta',
+              dica: '000'),
+          Editor(
+              controlador: _controladorCampoValor,
+              rotulo: 'Valor',
+              dica: '000',
+              icone: Icons.monetization_on),
           ElevatedButton(
-            onPressed: () {
-              final int? numeroConta =
-                  int.tryParse(_controladorCampoNumeroConta.text);
-              final double? valor =
-                  double.tryParse(_controladorCampoValor.text);
-
-              if (numeroConta != null && valor != null) {
-                final tranferenciaCriada = Transferencia(valor, numeroConta);
-                debugPrint('$tranferenciaCriada');
-              }
-            },
+            onPressed: () =>
+                _criaTransferencia(), //button de criar transferencia
             child: Text('Confirmar'),
           ),
         ],
+      ),
+    );
+  }
+
+  void _criaTransferencia() {
+    final int? numeroConta = int.tryParse(_controladorCampoNumeroConta.text);
+    final double? valor = double.tryParse(_controladorCampoValor.text);
+
+    if (numeroConta != null && valor != null) {
+      final transferenciaCriada = Transferencia(valor, numeroConta);
+      debugPrint('$transferenciaCriada');
+    }
+  }
+}
+
+class Editor extends StatelessWidget {
+  final TextEditingController controlador;
+  final String rotulo;
+  final String dica;
+  final IconData? icone; // usa-se o ? pois o campo não é obrigatório
+
+  const Editor(
+      {required this.controlador,
+      required this.rotulo,
+      required this.dica,
+      this.icone});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(24.0, 8.0, 24.0, 8.0),
+      child: TextField(
+        controller: controlador,
+        style: TextStyle(
+          fontSize: 20.0,
+        ),
+        decoration: InputDecoration(
+          icon: icone != null ? Icon(icone) : null, //icone //Operador Ternário
+          labelText: rotulo, //rotulo
+          hintText: dica, //dica
+        ),
+        keyboardType: TextInputType.number,
       ),
     );
   }
