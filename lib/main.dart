ef7c5717f686f5913079c18c228b95f6871821cd
milena <milenasantos@bemol.com.br>
2021-09-19 12:19:13 -0400

    Ajuste de tema do app

--- lib/main.dart
+++ lib/main.dart
@@ -11,9 +11,28 @@ class ByteBanckApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        body: ListaTransferencia(),
+      theme: ThemeData(
+        primaryColor: Colors.green[800],
+        accentColor: Colors
+            .green[300], //cor secundária https://material.io/resources/color/
+        //button,
+        elevatedButtonTheme: ElevatedButtonThemeData(
+            style: TextButton.styleFrom(
+          backgroundColor: Colors.green[300],
+          primary: Colors.grey[800],
+        )),
+        //input
+        inputDecorationTheme: InputDecorationTheme(
+            focusedBorder: UnderlineInputBorder(
+                borderSide: BorderSide(color: Colors.green)),
+            enabledBorder: UnderlineInputBorder(
+              borderSide: BorderSide(color: Colors.green),
+            ),
+            labelStyle: TextStyle(color: Colors.grey)),
+
+        //primarySwatch: Colors.green,
       ),
+      home: ListaTransferencia(),
     );
   }
 }
